db05596a6722bb285734ddcc099d285a73282b9c
Joel <zdztj_cn@sina.com>
2016-04-29 07:17:41 +0800

    add log information for servlet

--- Restaurant/src/main/java/com/netease/NoodlesServlet.java
+++ Restaurant/src/main/java/com/netease/NoodlesServlet.java
@@ -9,12 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import com.netease.Kitchen;
 public class NoodlesServlet extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(NoodlesServlet.class);
+
     protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer = response.getWriter();
 
         String vegetable = request.getParameter("vegetable");
+        if (vegetable == null){
+            vegetable = "Tomato";
+        }
+
+        logger.info("noodles with " + vegetable);
         
         String noodles = Kitchen.makeNoodles(vegetable);
         writer.println(noodles);
